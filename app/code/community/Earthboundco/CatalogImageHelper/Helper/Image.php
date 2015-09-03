97851bf3199947c5c084e60c84c324119a0772fe
Chad C. Davis <chad.davis@gmail.com>
2015-09-03 18:09:49 -0500

    Added Helper

---
+++ app/code/community/Earthboundco/CatalogImageHelper/Helper/Image.php
@@ -0,0 +1,40 @@
+<?php
+
+class Earthboundco_Catalog_Helper_Image extends Mage_Catalog_Helper_Image
+{
+    public function __toString()
+    {
+        try {
+            // TODO: CORE HACK
+            $model = $this->_getModel()->setQuality(95);
+
+            if ($this->getImageFile()) {
+                $model->setBaseFile($this->getImageFile());
+            } else {
+                $model->setBaseFile($this->getProduct()->getData($model->getDestinationSubdir()));
+            }
+
+            if ($model->isCached()) {
+                return $model->getUrl();
+            } else {
+                if ($this->_scheduleRotate) {
+                    $model->rotate($this->getAngle());
+                }
+
+                if ($this->_scheduleResize) {
+                    $model->resize();
+                }
+
+                if ($this->getWatermark()) {
+                    $model->setWatermark($this->getWatermark());
+                }
+
+                $url = $model->saveFile()->getUrl();
+            }
+        } catch (Exception $e) {
+            $url = Mage::getDesign()->getSkinUrl($this->getPlaceholder());
+        }
+        return $url;
+    }
+}
+
